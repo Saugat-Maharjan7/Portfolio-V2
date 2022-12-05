d0a31652cd135863caff4922560bc81247f75767
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2022-12-05 23:54:49 +0545

    three d environment fixed

--- pages/components/worldcanvas.js
+++ pages/components/worldcanvas.js
@@ -7,13 +7,30 @@ import { Suspense } from 'react';
 import gsap from 'gsap';
 import normal from "../../public/assets/textures/Normal.png"
 import height from "../../public/assets/textures/Height.jpg"
+import {OrbitControls,PerspectiveCamera} from "@react-three/drei"
 
+
+function Three(){
+    return(
+        <>
+        <PerspectiveCamera makeDefault position={[0,5,5]}/>
+        <OrbitControls></OrbitControls>
+       <pointLight position={[10, 7, 32]} />
+       {/* <ambientLight args={["#ffffff",1]}></ambientLight> */}
+    <mesh>
+      <sphereGeometry args={[2,64,100]}/>
+      <meshPhongMaterial color="#1f1f1f" />
+    </mesh>
+        </>
+    )
+}
 
 function World(){
     return(
         <>
         <Canvas id="threed-container" className="webgl" style={{position:"fixed"}}>
             <Suspense>
+                <Three/>
         </Suspense>
         </Canvas>
         <Script>{
