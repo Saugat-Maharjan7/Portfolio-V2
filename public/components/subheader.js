4a8e52f2a7f5fdcc701c550d51dff67c0ff89072
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2023-03-18 22:48:41 +0545

    back button

--- public/components/subheader.js
+++ public/components/subheader.js
@@ -26,11 +26,11 @@ function subhead(){
               <div className="container">
                 <div className="d-flex flex-row justify-content-between align-items-center">
                   <Link className="Logo d-flex flex-row align-items-center justify-content-center" style={{textDecoration:"none",color:"white"}} href="/" id="home">
-                    <div className="d-flex flex-row align-items-center justify-content-center"  >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-</svg>
-                    </div>
+                    <div className="d-flex flex-row align-items-center justify-content-center btn-contain"  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+</svg> 
+                    </div><span>Back</span>
                   </Link>
                   
                   <div className="links d-flex flex-row">
