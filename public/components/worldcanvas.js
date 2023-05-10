15e2ddcb34a7eb52637357b53d45ce2b018a086f
Shirish Shakya <84584836+Shirish-creator@users.noreply.github.com>
2023-05-10 17:03:41 +0545

    ufo added

--- public/components/worldcanvas.js
+++ public/components/worldcanvas.js
@@ -1,5 +1,5 @@
 import Script from 'next/script'
-import { Canvas } from '@react-three/fiber';
+import { Canvas, useLoader  } from '@react-three/fiber';
 import { Suspense } from 'react';
 
 import { EffectComposer, Bloom, Noise } from 'postprocessing';
@@ -16,21 +16,30 @@ import {OrbitControls,PerspectiveCamera, useTexture, Stars  } from "@react-three
 
 import React, { useRef,useState  } from "react";
 import { useFrame,useThree } from '@react-three/fiber';
-import { MeshStandardMaterial, RepeatWrapping } from 'three';
+import { DoubleSide, MeshStandardMaterial, RepeatWrapping } from 'three';
 
 import {useEffect} from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { useLoader } from '@react-three/fiber';
 
 function Three(){
 
+    const projects = useTexture("../me.png");
 
     let starref=useRef()
 
     let mousesathover=useRef(false)
-    // const earth = useLoader(GLTFLoader, '/earth.glb');
+    const ufo = useLoader(GLTFLoader, '/ufo.glb');
 
-    let earthref=useRef()
+    let uforef=useRef();
+    const [isHovered, setHovered] = useState(false);
+
+  const handlePointerOver = () => {
+    setHovered(true);
+  };
+
+  const handlePointerOut = () => {
+    setHovered(false);
+  };
 
     const scrollable=document.querySelector('main')
 
@@ -49,6 +58,12 @@ function Three(){
 
 
     const material = new MeshStandardMaterial({ color: (0x1a1a1a),  normalMap: normal,heightTextureMap:height})
+
+    // const squarematerial=new MeshBasicMaterial({
+    //     map: texture,
+    //     side: DoubleSide, // Enable double-sided rendering
+    //   });
+
     material.normalMap.repeat.set(8, 8)
     material.normalMap.repeat.set(8, 8)
     material.metalness = 0.463;
@@ -58,7 +73,7 @@ function Three(){
         scrollable.addEventListener('scroll',(e)=>{
             let t=scrollable.scrollTop;
         
-            let o=1
+            let o=1;
             sphereRef.current.position.z=t*0.8;
             sphereRef.current.rotation.z +=t*10;
             starref.current.position.z=t*0.5
@@ -83,13 +98,13 @@ function Three(){
 
         starref.current.rotation.y+=0.0002
 
-        if (earthref.current) {
+        if (uforef.current) {
             // Use a sine wave to move the model up and down
             const time = performance.now() / 1000;
-            earthref.current.position.y = Math.sin(time) * 0.1+2;
+            uforef.current.position.y = Math.sin(time) * 0.1+2;
           }
 
-          //this is the hover on earth        
+          //this is the hover on ufo        
         sphereRef.current.rotation.y += 0.001;
         pointLightRef.current.position.set(
         pointLightRef.current.position.x,
@@ -97,23 +112,57 @@ function Three(){
         pointLightRef.current.position.z,)
 
         camera.lookAt(sphereRef.current.position);
+
+        if (uforef.current) {
+            const scale = isHovered ? 0.8 : 0.7;
+            uforef.current.scale.x = uforef.current.scale.y = uforef.current.scale.z += (scale - uforef.current.scale.x) * 0.05;
+          }
     });
+
+    const square1Ref = useRef();
+    const square2Ref = useRef();
+    const square3Ref = useRef();
+
+    
 
     return(
 
         <>
-        <PerspectiveCamera ref={cameraref} makeDefault position={[0,1,8]}/>
+        <PerspectiveCamera ref={cameraref} makeDefault position={[0,3,8]}/>
        <pointLight ref={pointLightRef}  position={[1000, 800, -900]} intensity= {(10)} color={(0xFB722E)}/>
        <pointLight   position={[-2000, 900, -900]} intensity= {(10)} color={(0xC25FFD)}/>
         <OrbitControls></OrbitControls>
        
 
-       {/* <primitive ref={earthref} position={[20,2.5,2]} rotation={[19,-500,-10]} scale={[1,1,1]} object={earth.scene} /> */}
+       <primitive ref={uforef} position={[6,1,1]} 
+       rotation={[0.1,-10,0]} 
+       onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
+       scale={[1,1,1]}
+
+
+       object={ufo.scene} />
 
         {/* this is the sphere world */}
+
     <mesh ref={sphereRef} material={material}>
       <sphereGeometry args={[2.2,120,100]}/>
     </mesh>
+{/*     
+    <mesh ref={square1Ref} position={[-5, 0, 6]}>
+        <planeGeometry args={[1, 1]} />
+        <meshBasicMaterial map={projects} side={DoubleSide} />
+      </mesh>
+
+      <mesh ref={square2Ref} scale={3} position={[0, 5, 2]}>
+        <planeGeometry args={[1, 1]} />
+        <meshBasicMaterial map={projects}  side={DoubleSide} />
+      </mesh>
+
+      <mesh ref={square2Ref} position={[2, -5, 0]}>
+        <planeGeometry args={[1, 1]} />
+        <meshBasicMaterial map={projects}  side={DoubleSide} />
+      </mesh> */}
         <Stars ref={starref} />
         </>
     )
@@ -138,6 +187,8 @@ function World(){
 
            
           }}>
+            
+
         <Canvas  id="threed-container" className="webgl" style={{position:"fixed",zIndex:1}}>
             <Suspense>
                 
