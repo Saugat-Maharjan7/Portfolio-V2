5aa90d0a9147b1f204c5f10aba3307c5de4f00b8
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2023-11-16 10:21:12 +0545

    hero of super krishak

--- pages/projects/SuperKrishak.js
+++ pages/projects/SuperKrishak.js
@@ -6,6 +6,8 @@ import Head from "next/head";
 import StarsCanvas from "../../public/components/StarsCanvas"
 import Grain from "../../public/assets/textures/Grain.png"
 import Image from "next/image";
+import OrangeBall from "../../public/assets/textures/ORANGEBALL.png";
+import PJIMG from "../../public/assets/projects/trctor.png";
 
 import { projects } from "../../public/scripts/Datas/projects";
 
@@ -27,18 +29,35 @@ function SuperKrishak(){
             <Mouse></Mouse>
             <SubHead></SubHead>
             <StarsCanvas starColor="#AFFFE4" starSize={1.5} numStars={150}/>
+            <Image src={Grain} alt="Grain Texture" style={{width:'100%',height:'100%',position:'absolute',zIndex:3,mixBlendMode:'overlay',opacity:0.5,pointerEvents:'none'}}></Image>
+
             <main className="BGProjectsSuperKrishak100" style={{height:'100vh',width:'100%'}}>
-                <Image src={Grain} alt="Grain Texture" style={{width:'100%',height:'100vh',position:'absolute',zIndex:3,mixBlendMode:'overlay',opacity:0.5,pointerEvents:'none'}}></Image>
-            <section className="projectHero">
+            <section className="projectHero position-relative PBl " >
                 <div className="projectContainer d-flex flex-column MTfxl GAPxxl">
+                    <Image src={OrangeBall} className="position-absolute" style={{right:0,top:"-70px",zIndex:0,pointerEvents:'none',opacity:0.8}} alt="OrangeBall"></Image>
                     <div className="d-flex flex-row MTxxl justify-content-between"> 
                     <h1 className="text-uppercase projectTitle ">{project.title }</h1>
                     <span className="projectYear">{project.finishedYear}</span>
                     </div>
                     
-            <h1 className="projectLeadingTitle text-uppercase" >{'\u00A0\u00A0\u00A0\u00A0'}{project.LeadingTitle }</h1></div>
+            <h1 className="projectLeadingTitle text-uppercase" >{'\u00A0\u00A0\u00A0\u00A0'}{project.LeadingTitle }</h1>
+            <div className="d-flex justify-content-between projectRepsonibilityAndImage GAPl">
+                <div className="d-flex flex-column GAPl projectResponsibility">
+                    <p className="FONTNEXA ">My Responsibilities:</p>
+                <ul className="d-flex flex-column GAPs"  style={{listStyle:'none',padding:0,margin:0}}>
+                {project && project.responsibilities.map((responsibility, index) => (
+                        <li className="projectResponsibilities FONTNEXA" key={index}>{responsibility}</li>
+                    ))}
+                     </ul></div>
+                
+                     <div className="projectHeroImage">
+                        <Image src={PJIMG}  alt="projectIMG"></Image>
+                     </div>
+            </div>
+            </div>
             
             </section>
+            <section className="BGBrandBlack500" style={{height:'500px'}}></section>
             </main>
         
         
--- public/scripts/Datas/projects.js
+++ public/scripts/Datas/projects.js
@@ -4,34 +4,39 @@ export const projects = [
     title: 'Super Krishak',
     link: '/projects/SuperKrishak',
     LeadingTitle: 'MAKING FARMING LEARNING EASIER THAN EVER',
-    finishedYear: 2023 // Add the finished year here for the first project
+    finishedYear: 2023,
+    responsibilities: ['UX Research', 'Branding', 'Ui Design', 'Design System', 'Art Direction']
   },
   {
     id: 2,
     title: 'Upkite',
     link: '/projects',
     LeadingTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    finishedYear: 2023 // Add the finished year here for the second project
+    finishedYear: 2023,
+    responsibilities: ['Developing website frontend', 'Implementing backend functionality']
   },
   {
     id: 3,
     title: 'Clean Eats',
     link: '/projects',
     LeadingTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    finishedYear: 2022 // Add the finished year here for the third project
+    finishedYear: 2022,
+    responsibilities: ['Creating healthy recipe database', 'Marketing and promotion']
   },
   {
     id: 4,
     title: 'Phone Care Clinic',
     link: '/projects',
     LeadingTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    finishedYear: 2023 // Add the finished year here for the fourth project
+    finishedYear: 2023,
+    responsibilities: ['Establishing repair procedures', 'Training new technicians']
   },
   {
     id: 5,
     title: 'ISEAA',
     link: '/projects',
     LeadingTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    finishedYear: 2023 // Add the finished year here for the fifth project
+    finishedYear: 2023,
+    responsibilities: ['Conducting environmental impact assessments', 'Data analysis and reporting']
   },
 ];
