44caefceb4824ca12ee025d45f58c569a60edfc0
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2023-11-21 11:23:07 +0545

    drop down fixed

--- public/components/Dropdown.js
+++ public/components/Dropdown.js
@@ -1,59 +1,77 @@
-//this is the follow mouse cursor component
+import { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { projects } from '../scripts/Datas/projects';
 
+function Dropdown() {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-import Script from 'next/script'
-import {useEffect} from 'react'
-import React, { useRef,useState  } from "react";
-// import Menu from './menu';
-import Link from "next/link";
-import {projects} from '../scripts/Datas/projects'; //projects database
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    
+    document.addEventListener('mousedown', handleOutsideClick);
 
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
 
+  useEffect(() => {
+    let scrollable = document.querySelector('main');
+    
+    scrollable.addEventListener('scroll', () => {
+      let t = scrollable.scrollTop;
+      if (t > 0) {
+        setIsOpen(false);
+      }
+    });
+  });
 
-function Dropdown(){
-
-   const [isOpen,setIsOpen]=useState(false)
-
-    return(
-
-      
-        <div style={{position:'relative'}}>
-        
-        <button 
-        onClick={()=>setIsOpen((prev)=>!prev)}
-
-        className="btn btn-down" 
-        
-        id="works"> 
+  return (
+    <div style={{ position: 'relative' }}>
+      <motion.button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="btn btn-down"
+        id="works"
+        transition={{ duration: 0.2 }}
+      >
         projects
-
-        {isOpen &&(
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className={`bi ${isOpen ? 'bi-chevron-up' : 'bi-chevron-down'}`}
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d={isOpen ? "M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" : "M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"}
+          />
         </svg>
-         )}
+      </motion.button>
 
-        {!isOpen &&(
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-down" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-          </svg>
-         )}
-        
-      </button>
-     
-     {isOpen &&(
-      <div>
-        
-          <ul className='dropdown_menu'>
-            {projects.map(item=>(
-                 <Link href={item.link} key={item.id} className='dropdown_item'>{item.title}</Link>
-            ))}
-            </ul>
-       
-      </div>
-     )}
-      </div>
-    )
+      <motion.div
+        ref={dropdownRef}
+        initial={{ opacity: 0, height: 0, overflow: 'hidden' }}
+        animate={isOpen ? { opacity: 1, height: 'auto', overflow: 'visible' } : { opacity: 0, height: 0 }}
+        transition={{ duration: 0.3 }}
+      >
+        <ul className='dropdown_menu'>
+          {projects.map((item) => (
+            <Link href={item.link} key={item.id} className='dropdown_item'>
+              {item.title}
+            </Link>
+          ))}
+        </ul>
+      </motion.div>
+    </div>
+  );
 }
 
 export default Dropdown;
