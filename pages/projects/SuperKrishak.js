6eb8e1a69798a9f4192af8906aabcaf0bcd6e39d
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2023-11-29 10:11:21 +0545

    speed updated

--- pages/projects/SuperKrishak.js
+++ pages/projects/SuperKrishak.js
@@ -261,7 +261,7 @@ function SuperKrishak(){
                     height={465}
                     layout="responsive"
                     className="parallax-image"
-                    data-speed="20" // Speed of parallax effect
+                    data-speed="0.2" // Speed of parallax effect
                      style={{ transform: 'translateY(0)' ,transition:'ease-in-out 0.5s all'}}
                     ></Image>
                      
@@ -317,7 +317,7 @@ function SuperKrishak(){
                     height={814}
                     layout="responsive"
                     className="parallax-image"
-                    data-speed="20" // Speed of parallax effect
+                    data-speed="0.2" // Speed of parallax effect
                     style={{ transform: 'translateY(0)' ,transition:'ease-in-out 0.5s all'}}
                     ></Image>
                     </div>
@@ -370,7 +370,7 @@ function SuperKrishak(){
                     height={814}
                     layout="responsive"
                     className="parallax-image"
-                    data-speed="20" // Speed of parallax effect
+                    data-speed="0.2" // Speed of parallax effect
                     style={{ transform: 'translateY(0)' ,transition:'ease-in-out 0.5s all'}}
                     ></Image>
                     </div>
