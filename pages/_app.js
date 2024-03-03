d278e4f1870841c8aa607263aff59a36b80f1ba1
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-03-03 15:54:02 -0500

    loader moded

--- pages/_app.js
+++ pages/_app.js
@@ -31,9 +31,9 @@ function MyApp({ Component, pageProps }) {
     return (
       <div className="loader-wrapper">
         <div>
-          <img src="/assets/rocketslow.gif" alt="Rocket GIF" />
+          <img style={{mixBlendMode:'lighten'}} src="/assets/loom.gif" alt="Rocket GIF" />
           <div className="d-flex flex-column justify-content-between">
-            <div className="d-flex flex-row justify-content-between"><p>Initiating Launch</p>
+            <div className="d-flex flex-row justify-content-between"><p>Loading</p>
             <p>{`${loadingProgress}%`}</p></div>
             
             <div className="load-bar">
