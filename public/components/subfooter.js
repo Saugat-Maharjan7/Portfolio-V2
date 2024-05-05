8af2cab2563dc3df8733e5f8f1828598fc5bcc6f
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-05-05 17:25:25 -0400

    tokens added

--- public/components/subfooter.js
+++ public/components/subfooter.js
@@ -10,21 +10,11 @@ function SubFooter(){
     const year = currentDate.getFullYear();
 
 
-  //for time
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       const date = new Date();
-//       const Year = date.toDateString();
-//       setTime(Year);
-//     }, 1000);
-
-//     return () => clearInterval(interval);
-//   }, []);
 
     return(
         <footer className=""> 
         <div className="container">
-          <div className="copyrights d-flex flex-row justify-content-between">
+          <div className="newcontainer d-flex flex-row justify-content-between">
             <span>Designed and Developed with love and threejs</span>
             <p>© Shirish Shakya.<span className="dated">{year} </span></p>
             <span>All Rights Reserved</span>
