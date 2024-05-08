f8fb7f7e11a46ea03764991e3b1c10db0261910a
Shirish-creator <84584836+Shirish-creator@users.noreply.github.com>
2024-05-08 01:02:59 -0400

    reverted back iframe

--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,31 @@ import World from "../public/components/worldcanvas"
 import Mouse from "../public/components/Mouse"
 import Head from "next/head"
 import { easeIn, motion as m } from "framer-motion"
+import { Spring } from "@react-spring/core"
+import styles from '../styles/Custom_pages/custom-components/ToggleSwitch.module.css';
 import Sidenavigation from "../public/components/side-navigation";
+import Typewriter from 'typewriter-effect';
 import {projects} from '../public/scripts/Datas/projects'; //projects database
 import BGERASER from "../public/components/BgEraserSection";
 import React from 'react';
 import Button from "../public/components/Button";
 import Spline from '@splinetool/react-spline';
 // import { ReactLenis, useLenis } from '@studio-freight/react-lenis'
-import { Source } from "three"
-import EmblemScene from "../public/components/EmblemScene"
+
 
 
 export default function Home() {
 
-   
+    //button icons
+    // const lenis = useLenis(({ scroll }) => {
+    //   // called every scroll
+    // })
   
     const mainRef = useRef(null);
+    // useLenis(mainRef);
 
     let exploreref=useRef()
-    const landingTitle="CRAFTING TOMORROW'S INNOVATION."
+    const landingTitle="DESIGNING TOMORROW: UNVEILING MY VISIONARY WORLD"
     const [isToggled, setToggled] = useState(false);
     const [time, setTime] = useState('');
 
@@ -130,16 +136,13 @@ export default function Home() {
 
             
       {/* <World/> */}
-      {/* <EmblemScene/> */}
-
+      
 
         <main ref={mainRef} className={checked ? 'main-inactive' : ''}>
         <Header toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen}></Header>
 
-          <div className="hero-wrapper position-relative">
-
-            <section className="w-100 hero-container position-relative" style={{top: 0}} >
-
+          <div className="hero-wrapper position-relative">           
+            <section className="w-100 hero-container" style={{top: 0}} >
               <div className="container position-relative">
                 <m.div 
                  initial={{
@@ -162,7 +165,7 @@ export default function Home() {
                   <img className="sun_light" src="/assets/sun light.png" alt="sun light" />
                 <div
                 >
-                  <iframe className="particles-sphere" style={{position:'absolute',zIndex:2
+                  <iframe className="particles-sphere" style={{position:'absolute',
                   right:'0%',
                   bottom:'0%',
                   width: '100%', // Set initial width to 100%
@@ -179,13 +182,14 @@ export default function Home() {
 
                   frameBorder="0" 
                   allowTransparency="true" >
-                  </iframe> 
+                  </iframe>
+                  {/* <Portal/>  */}
                  
                   <div style={{width:"200px",height:'50px',background:"#161515",position:'absolute',bottom:'2%',right:"20px",zIndex:'0'}}></div>
                   </div>
 
                   <div className="hero-title d-flex flex-column">
-                    <span style={{fontFamily:"Monument Extended Bold", letterSpacing:0.8}}>SHIRISH SHAKYA </span>
+                    <span style={{fontFamily:"Monument Extended Bold", letterSpacing:0.8}}>SHIRISH SHAKYA</span>
                    
                     <h1 className="FONTMONUMENT" >
                       <div>
@@ -202,11 +206,39 @@ export default function Home() {
                           ))}
                           </div>
                     </h1>
+                    
+                      {/* <div className="TypeWriterWrapper ">
+                    <Typewriter
+                    style={{letterSpacing:0}}
+                        options={{
+                          strings: ['I design', 'I code','I create',''],
+                          autoStart: true,
+                          loop: true,
+                        }}
+                      />
+                      </div> */}
+                      
                       <p>Designer from Greater Toronto Area</p>
                       
                       
                   </div>
                   <div className="profile_info_top d-flex w-100" style={{position:'relative',zIndex:'1'}}>
+                    {/* <div className="profile_detail d-flex flex-row">
+                      <div className="profile_picture" />
+                      <p className="profile_details">Hey!, I’m a product designer based
+                        in Canada <span><svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g clipPath="url(#clip0_3249_7064)">
+<path fillRule="evenodd" clipRule="evenodd" d="M12.1043 18.7918C12.484 21.4036 11.9001 23.5128 10.3375 25.6768C9.98629 26.1619 9.61537 26.5525 9.18519 26.9695C9.183 26.3923 8.81427 26.1443 8.25899 26.017C10.5438 23.7586 11.4853 21.2675 11.786 18.7918H11.268C9.5583 18.6228 7.68396 21.5023 5.48478 22.2859C6.53169 20.2754 5.36187 20.0033 2.70618 20.8966C4.69246 18.5262 4.8856 17.6505 2.70618 17.9665C3.8321 17.115 4.87902 16.6255 5.86228 16.4258C3.81455 15.7739 1.99287 14.6765 0.454326 13.0085C3.34267 12.9032 2.63375 10.952 0.00439453 8.01317C3.50069 9.77119 5.34431 9.99287 4.77367 7.97586C5.80741 8.6738 7.13306 10.0104 8.52895 11.5073C7.99123 8.76379 7.58738 6.16516 7.77833 4.22058C9.1369 6.26831 10.6008 4.19643 12.0362 0C13.2455 3.82771 14.5449 5.95226 16.1888 4.40713C16.4017 6.11029 16.1734 7.93196 15.5874 9.8524L15.5501 11.3163C16.913 9.84582 18.2519 8.45652 19.3295 7.92757C18.6601 10.1421 21.0041 9.36955 24 8.01097C21.1643 11.1802 20.9405 12.8527 23.4359 12.969C22.3978 14.5844 20.5674 15.5676 18.4406 16.3117C19.3668 16.5772 20.293 17.0776 21.2192 17.8524C19.3032 17.6944 19.7048 18.9498 21.2741 20.969C18.8774 20.2667 17.6549 20.4576 18.4121 22.2859C15.5786 19.9155 13.5572 18.7676 12.3193 18.7918H12.1043Z" fill="#FF0000"/>
+</g>
+<defs>
+<clipPath id="clip0_3249_7064">
+<rect width="24" height="26.9695" fill="white"/>
+</clipPath>
+</defs>
+</svg>
+</span> who brings concepts,
+                        art, and ideas into reality.</p>
+                    </div> */}
                     <div className="social_links d-flex flex-column">
                       <span>shirish.shakya5@gmail.com</span>
                       <div className="time">{time} <span>Local Time</span></div>
@@ -214,7 +246,7 @@ export default function Home() {
                         <li ><a className='TXTBrandOrange500 SIZEF6' target="_blank" href="https://www.linkedin.com/in/shirish-shakya-0bb39a293">Linkedin</a></li>
                         <li><a className='TXTBrandOrange500 SIZEF6' target="_blank" href="https://www.behance.net/shirishshakya">Behance</a></li>
                         <li><a className='TXTBrandOrange500 SIZEF6' target="_blank" href="https://www.instagram.com/shakyastagram/?hl=en">Instagram</a></li>
-                        <li><a className='TXTBrandOrange500 SIZEF6' target="_blank" href="https://drive.google.com/file/d/1IpyPxFOvWcvw_49_wbatQUEVK4th5vK2/view?usp=drive_link">Resume</a></li>
+                        <li><a className='TXTBrandOrange500 SIZEF6' target="_blank" href="https://drive.google.com/file/d/1YVcCpCQQ6g_sIYKn2--uT22rAtxjbcNp/view?usp=sharing">Resume</a></li>
                       </ul>
                     </div>
                   </div>
@@ -227,19 +259,15 @@ export default function Home() {
               <div style={{borderTop:"1px solid white"}} className="border-l-r-b d-flex flex-row container-title">
                
                 <div className=" container-header d-flex align-items-center" >
-                  <h2 className="secton-header-h2">FEATURED PROJECTS </h2>
-
+                  <h2 className="secton-header-h2">FEATURED PROJECTS</h2>
                   
                 </div>
               </div>
             </div>
             <div className="container d-flex flex-row " style={{justifyContent: 'center'}}>
-              
               {/* projects container */}
               <ul className="border-l-r-b project-contents">
-
               {projects.filter(project => project.featured === true).map((project, index, array) => (
-
                 <React.Fragment key={project.id}>
                   <li  className="project d-flex" id={project.title} >
                     <div className="p-left d-flex flex-column">
@@ -296,15 +324,3 @@ export default function Home() {
    
   )
 }
-
-
-// export async function getServerSideProps(context){
-//   const res = await fetch(`https://eoa1wcoa1epng8i.m.pipedream.net`);
-//   const cmsdata = await res.json();
-  
-//   return {
-//     props: {
-//       cmsdata 
-//     }
-//   };
-// }
