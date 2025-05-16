4583878dd15f3714119302a2db417ef67ec76c44
unknown <Saugat-Maharjan7>
2025-05-16 13:39:35 +0545

    Added Meta Description

--- pages/index.js
+++ pages/index.js
@@ -113,7 +113,7 @@ export default function Home() {
         <Head>
         <meta property="og:image" content="/assets/websitebanner.jpg" />
         <meta property="og:image:width" content="1200" />
-          <meta property="og:image:height" content="630" />
+        <meta property="og:image:height" content="630" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -121,6 +121,7 @@ export default function Home() {
         <meta name="msapplication-TileColor" content="#A855F7" />
         <meta name="theme-color" content="#ffffff" />
         <title>Saugat Maharjan</title>
+        <meta name="description" content="Namaste! I'm Saugat Maharjan, a dedicated UI/UX Designer with an Engineering degree from Kathmandu, Nepal, and over two years of industry experience. My expertise spans the full design process—from user research to high-fidelity interfaces—focused on delivering visually compelling and functional digital experiences. My passion for design began during my undergraduate studies and has since evolved into a career driven by purpose and creativity." />
 
         </Head>
       
