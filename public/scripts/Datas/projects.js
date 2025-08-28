11a7b51c132a603dcd541f75f96d1fadd915c4bc
Saugat Maharjan <saugatmaharjan11@gmail.com>
2025-08-28 13:50:54 +0545

    Added Orion to Projects list and Testimonials of Udaan

--- public/scripts/Datas/projects.js
+++ public/scripts/Datas/projects.js
@@ -1,47 +1,53 @@
 export const projects = [
   {
     id: 1,
+    title: 'ORION System LLC',
+    link:"https://dribbble.com/shots/26051566-ORION-Systems-LLC",
+ 
+  },
+  {
+    id: 2,
     title: 'Udaan V2.0',
     link:"https://dribbble.com/shots/25920729-Udaan-V2-0",
  
   },
   {
-    id: 2,
+    id: 3,
     title: 'Online Attendance App',
     link: 'https://dribbble.com/shots/25606997-Online-Attendance-App',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Shanti Yoga Ashram',
     link: 'https://dribbble.com/shots/25606934-Shanti-Yoga-Ashram-Website',
   },
   {
-    id: 4,
+    id: 5,
     title: 'SCSS Consulting - Website',
     link: 'https://dribbble.com/shots/25606927-SCSS-Consulting-Revamp-Website',
   },
   {
-    id: 5,
+    id: 6,
     title: 'GlobalyHub - Company Website',
     link: 'https://dribbble.com/shots/25606893-GlobalyHub-Company-Website',
   },
   {
-    id: 6,
+    id: 7,
     title: 'Gamified Healthy Eating App',
     link: 'https://dribbble.com/shots/25606845-Gamified-Healthy-Eating-App',
   },
   {
-    id: 7,
+    id: 8,
     title: 'Nepawatch - Nepal Exclusive Watch',
     link: 'https://dribbble.com/shots/25606815-NepaWatch-Nepal-Exclusive-Watch',
   },
   {
-    id: 8,
+    id: 9,
     title: 'Trendy Florist',
     link: 'https://dribbble.com/shots/25606105-Trendy-Florist',
   },
   {
-    id: 9,
+    id: 10,
     title: 'GHUMFIR: Travel Companion',
     link: 'https://dribbble.com/shots/25606082-GHUMFIR-Travel-Companion',
   },
